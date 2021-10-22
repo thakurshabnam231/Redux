fd66fe7336ec7fc809ced27c9d249bf439bb508e
shabnam rani <88382451+thakurshabnam231@users.noreply.github.com>
2021-10-22 02:16:13 -0700

    redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
+import{useSelector,useDispatch} from 'react-redux'
+import{increment,decrement} from './actions/index'
 import './App.css';
 
 function App() {
+ const myState= useSelector((state)=>state.changeNumber)
+ const dispatch=useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+    <h1>increment/decrement counter using redux</h1>
+     <div class="span">
+     <button onClick={()=>dispatch(increment)}>
+     <span>-</span></button>
+     <input type="text" value={myState} ></input>
+     <button  onClick={()=>dispatch(decrement)}>
+     <span>+</span>
+     </button>
+     </div>
     </div>
+    
   );
 }
 
---
+++ src/reducer/upDown.js
@@ -0,0 +1,9 @@
+const initialState = 0;
+const changeNumber = (state=initialState,action) => {
+    switch (action.type) {
+        case 'increment': return state + 1;
+        case 'decrement': return state - 1;
+        default: return state
+    }
+}
+export default changeNumber;
